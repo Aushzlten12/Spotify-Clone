7bb9c6c1037fd88f5cc0e0757eeb0aff24136a06
Aushzlten12 <josepachas1604@gmail.com>
2024-05-04 13:54:07 -0500

    fetch songs

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,11 +1,27 @@
 "use client";
 
+import useAuthModal from "@/hooks/useAuthModal";
+import useUploadModal from "@/hooks/useUploadModal";
+import { useUser } from "@/hooks/useUser";
+import { Song } from "@/types";
 import { AiOutlinePlus } from "react-icons/ai";
 import { TbPlaylist } from "react-icons/tb";
+import MediaItem from "./MediaItem";
 
-const Library = () => {
+interface LibraryProps {
+  songs: Song[];
+}
+
+const Library: React.FC<LibraryProps> = ({ songs }) => {
+  const authModal = useAuthModal();
+  const uploadModal = useUploadModal();
+  const { user } = useUser();
   const onClick = () => {
-    // Handle upload later
+    if (!user) {
+      return authModal.onOpen();
+    }
+    // PENDING: Check for subscriptions
+    return uploadModal.onOpen();
   };
   return (
     <div className="flex flex-col">
@@ -22,7 +38,11 @@ const Library = () => {
         />
       </div>
 
-      <div className="flex flex-col gap-y-2 mt-4 px-3">List of Songs!</div>
+      <div className="flex flex-col gap-y-2 mt-4 px-3">
+        {songs.map((item) => (
+          <MediaItem onClick={() => {}} key={item.id} data={item} />
+        ))}
+      </div>
     </div>
   );
 };
